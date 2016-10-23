ec7aed3961eece04fe836251535be690d264b073
Adrian L. Shaw <adrianlshaw@gmail.com>
2016-10-23 16:49:16 -0700

    Built an amazing script for kernel debugging

--- debug-headless.sh
+++ debug-headless.sh
@@ -1,7 +1,10 @@
 echo "Use Ctrl A h to quit"
 sleep 2
 
-kvm -s -kernel arch/x86/boot/bzImage \
+tmux new-session -s asdf -n myWindow -d
+
+
+tmux send-keys -t asdf:myWindow.0 "kvm -s -kernel arch/x86/boot/bzImage \
   -drive file=wheezy.img,if=virtio \
   -net nic,model=virtio,macaddr=52:54:00:12:34:56 \
   -net user,hostfwd=tcp:127.0.0.1:4444-:22 \
@@ -10,4 +13,12 @@ kvm -s -kernel arch/x86/boot/bzImage \
   -device virtio-serial-pci \
   -device virtconsole,chardev=stdio \
   -mon chardev=stdio \
-  -display none
+  -display none" C-j
+
+tmux split-window -h
+
+tmux send-keys -t 1 '$PWD/start-gdb.sh && pkill tmux' C-j
+
+tmux select-window -t asdf:mywindow
+
+tmux attach -t asdf
