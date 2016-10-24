5f76a4504ca487d210a3163e10edc5426dce003c
Adrian L. Shaw <adrianlshaw@gmail.com>
2016-10-24 21:44:20 +0100

    Removed /bin/sh from init

--- boot-headless.sh
+++ boot-headless.sh
@@ -5,7 +5,7 @@ kvm -kernel arch/x86/boot/bzImage \
   -drive file=wheezy.img,if=virtio \
   -net nic,model=virtio,macaddr=52:54:00:12:34:56 \
   -net user,hostfwd=tcp:127.0.0.1:4444-:22 \
-  -append 'root=/dev/vda console=hvc0 init=/bin/sh' \
+  -append 'root=/dev/vda console=hvc0' \
   -chardev stdio,id=stdio,mux=on,signal=off \
   -device virtio-serial-pci \
   -device virtconsole,chardev=stdio \
