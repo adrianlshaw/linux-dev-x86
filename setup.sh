688c404e55b1083b4b0dbb2bfe8ba04717582bb7
Adrian L. Shaw <adrianlshaw@gmail.com>
2018-01-16 19:15:58 +0000

    Automated test attempt #1

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 INSTALLDEB=/opt/wheezy
 mkdir $INSTALLDEB
-sudo debootstrap --arch=amd64 wheezy $INSTALLDEB && \
-sudo sed -i '/^root/ { s/:x:/::/ }' $INSTALLDEB/etc/passwd && \
-echo 'V0:23:respawn:/sbin/getty 115200 hvc0' | sudo tee -a $INSTALLDEB/etc/inittab && \
-printf '\nauto eth0\niface eth0 inet dhcp\n' | sudo tee -a $INSTALLDEB/etc/network/interfaces && \
-sudo mkdir $INSTALLDEB/root/.ssh/ && \
-cat ~/.ssh/id_rsa.pub | sudo tee $INSTALLDEB/root/.ssh/authorized_keys
+debootstrap --arch=amd64 wheezy $INSTALLDEB && \
+sed -i '/^root/ { s/:x:/::/ }' $INSTALLDEB/etc/passwd && \
+echo 'V0:23:respawn:/sbin/getty 115200 hvc0' | tee -a $INSTALLDEB/etc/inittab && \
+printf '\nauto eth0\niface eth0 inet dhcp\n' | tee -a $INSTALLDEB/etc/network/interfaces && \
+mkdir $INSTALLDEB/root/.ssh/ && \
+cat ~/.ssh/id_rsa.pub | tee $INSTALLDEB/root/.ssh/authorized_keys
 
 # Build a disk image
 dd if=/dev/zero of=wheezy.img bs=1M seek=4095 count=1
 mkfs.ext4 -F wheezy.img
-sudo mkdir -p /mnt/wheezy
-sudo mount -o loop wheezy.img /mnt/wheezy
-sudo cp -a $INSTALLDEB/. /mnt/wheezy/.
-sudo umount /mnt/wheezy
+mkdir -p /mnt/wheezy
+mount -o loop wheezy.img /mnt/wheezy
+cp -a $INSTALLDEB/. /mnt/wheezy/.
+umount /mnt/wheezy
