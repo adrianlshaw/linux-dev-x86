1491f76a0bb193ce37b687cf4fa267a822cea7ca
Adrian L. Shaw <adrian.shaw@arm.com>
2018-01-18 23:15:29 +0000

    Attempt #6

--- ci.sh
+++ ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+echo "Starting the VM"
 ./boot-headless-network.sh > /dev/null 2>&1  &
 sleep 6
+echo "Testing the network"
 ssh root@127.0.0.1 -p 4444  -o StrictHostKeyChecking=no -t "echo hello"
 exit $?
--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ sed -i '/^root/ { s/:x:/::/ }' $INSTALLDEB/etc/passwd && \
 echo 'V0:23:respawn:/sbin/getty 115200 hvc0' | tee -a $INSTALLDEB/etc/inittab && \
 printf '\nauto eth0\niface eth0 inet dhcp\n' | tee -a $INSTALLDEB/etc/network/interfaces && \
 mkdir $INSTALLDEB/root/.ssh/ && \
-cat ~/.ssh/id_rsa.pub | tee $INSTALLDEB/root/.ssh/authorized_keys
+ssh-keygen -b 2048 -t rsa -f $PWD/sshkey -q -N "" && \
+cat sshkey.pub | tee $INSTALLDEB/root/.ssh/authorized_keys
 
 # Build a disk image
 dd if=/dev/zero of=wheezy.img bs=1M seek=4095 count=1
