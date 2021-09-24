5ba6b81217ffd0984b2fc90e05ab128bf5f9e629
Adrian L. Shaw <adrianlshaw@gmail.com>
2021-09-24 15:06:50 +0100

    Fix setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-set -e
+set -ex
+
+DEBVERSION="buster"
+
+wget https://ftp-master.debian.org/keys/release-10.asc -qO- | gpg --import --no-default-keyring --keyring ./debian-release-10.gpg
 
 command -v debootstrap >/dev/null 2>&1 || { echo >&2 "Install debootstrap.  Aborting."; exit 1; }
 
-INSTALLDEB=/opt/buster
+INSTALLDEB=/opt/$DEBVERSION
 mkdir $INSTALLDEB
-sudo debootstrap --arch=amd64 buster $INSTALLDEB && \
+sudo debootstrap --keyring=./debian-release-10.gpg --arch=amd64 $DEBVERSION $INSTALLDEB && \
 sudo sed -i '/^root/ { s/:x:/::/ }' $INSTALLDEB/etc/passwd && \
 echo 'V0:23:respawn:/sbin/getty 115200 hvc0' | sudo tee -a $INSTALLDEB/etc/inittab && \
 printf '\nauto eth0\niface eth0 inet dhcp\n' | sudo tee -a $INSTALLDEB/etc/network/interfaces && \
@@ -13,10 +17,10 @@ sudo mkdir $INSTALLDEB/root/.ssh/ && \
 cat ~/.ssh/id_rsa.pub | sudo tee $INSTALLDEB/root/.ssh/authorized_keys
 
 # Build a disk image
-dd if=/dev/zero of=buster.img bs=1M seek=4095 count=1
-mkfs.ext4 -F buster.img
-sudo mkdir -p /mnt/buster
-sudo mount -o loop buster.img /mnt/buster
-sudo cp -a $INSTALLDEB/. /mnt/buster/.
-sudo echo "mount -t securityfs securityfs /sys/kernel/security" >> /mnt/buster/root/.bashrc
-sudo umount /mnt/buster
+dd if=/dev/zero of=$DEBVERSION.img bs=1M seek=4095 count=1
+mkfs.ext4 -F $DEBVERSION.img
+sudo mkdir -p /mnt/$DEBVERSION
+sudo mount -o loop $DEBVERSION.img /mnt/$DEBVERSION
+sudo cp -a $INSTALLDEB/. /mnt/$DEBVERSION/.
+sudo echo "mount -t securityfs securityfs /sys/kernel/security" >> /mnt/$DEBVERSION/root/.bashrc
+sudo umount /mnt/$DEBVERSION
